d17bda65c7b4890b7ecbe4e65579d3132759238d
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-07-26 19:26:09 +0300

    Add ConfigurationServiceProvider to manage application configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\ConfigurationServiceProvider;
 
 return [
     AppServiceProvider::class,
+    ConfigurationServiceProvider::class
 ];
